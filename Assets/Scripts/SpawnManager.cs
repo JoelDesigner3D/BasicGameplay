4c1915b00f037802c1f60f47fcc9fe557cfaf419
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-01-03 14:43:01 +0100

    Camera Ortho
    Class name correction

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpownManager : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
