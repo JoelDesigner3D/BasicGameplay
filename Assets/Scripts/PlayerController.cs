92779cff6ebd3c9d8401813ad23f5d4b5f8cff28
Joel <120654417+JoelDesigner3D@users.noreply.github.com>
2023-01-04 05:44:22 +0100

    New Input System

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
@@ -10,7 +11,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10f;
 
+    private float movementX;
+    //private float movementY;
+
+    public Rigidbody body;
+
     public GameObject projectilePrefab;
+
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +38,42 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
 
+        /*******************************
+         * Old Input System
+        
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime) ;
-
+       
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // launch a projectile from the player
             print("launch projectile");
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
+        *******************************/
+    }
+
+    public void OnMove(InputValue movementValue)
+    {
+
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        //movementY = movementVector.y;
+
+        Debug.Log("movementX : " + movementVector.x);
+
+        body.velocity = new Vector3(movementX * speed, body.velocity.y, body.velocity.z);
+
+        // transform.Translate(Vector3.right * movementVector.x * speed * Time.deltaTime);  
+
+    }
+
+    public void OnFire() // Space key
+    {
+        // launch a projectile from the player
+        Debug.Log("launch projectile");
+
+        // Create a new projectile and add it to the scene
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
     }
 }
